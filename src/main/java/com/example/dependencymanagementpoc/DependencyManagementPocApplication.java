baa466d5d36582f858ebcead4f564c4f19ea7e62
Florian McKee <Florian.McKee@sysco.com>
2022-09-29 14:39:26 -0500

    initial commit

---
+++ src/main/java/com/example/dependencymanagementpoc/DependencyManagementPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.dependencymanagementpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyManagementPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DependencyManagementPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dependencymanagementpoc/DependencyManagementPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dependencymanagementpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyManagementPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
